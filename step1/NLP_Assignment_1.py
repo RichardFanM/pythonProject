a1585c94aa4d13e24a910f71c4e40368507ead0f
mengchenfan <fanm@uab.edu>
2022-09-07 15:24:35 -0500

    assignment1

--- step1/NLP_Assignment_1.py
+++ step1/NLP_Assignment_1.py
@@ -1,12 +1,17 @@
 import io
 
-import nltk.corpus
 import tokenizers.pre_tokenizers
 import evaluate
 from evaluate import load
 
-f = open("generifs1000.tsv", "r")
-print(f.read())
+import csv
+
+with open("generifs1000.tsv") as file:
+    tsv_file = csv.reader(file, delimiter="\t")
+
+    for line in tsv_file:
+        print(line[4])
+
 
 corpus = []
 max1 = []
