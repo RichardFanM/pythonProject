d8f06f8db1945df0740c041c3f15e70753f64a08
fanm <fanm@uab.edu>
2022-09-07 17:18:06 -0500

    assignment

--- step1/NLP_Assignment_1.py
+++ step1/NLP_Assignment_1.py
@@ -1,18 +1,31 @@
 import io
 
-import tokenizers.pre_tokenizers
+import tokenizers.pre_tokenizers as pt
 import evaluate
 from evaluate import load
 
 import csv
 
+corpus = []
+max1 = []
+max2 = []
+
+is_FirstLine = False
+
 with open("generifs1000.tsv") as file:
     tsv_file = csv.reader(file, delimiter="\t")
 
     for line in tsv_file:
-        print(line[4])
+        if is_FirstLine:
+            corpus.append(line[4])
+        is_FirstLine = True
+
+print("Corpus size: " + str(len(corpus)))
 
 
-corpus = []
-max1 = []
-max2 = []
+def tokenize():
+    bpt = pt.BertPreTokenizer()
+    print(bpt.pre_tokenize_str("this is a test"))
+
+
+tokenize()
