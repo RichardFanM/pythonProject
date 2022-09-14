e668f40ca60a285213df52d8f19cdb481b1772c7
fanm <fanm@uab.edu>
2022-09-14 08:01:42 -0500

    assignment

--- step1/NLP_Assignment_1.py
+++ step1/NLP_Assignment_1.py
@@ -1,10 +1,13 @@
+import os
+import string
+
+os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
+
 import io
 
 import tokenizers.pre_tokenizers as pt
 import evaluate
 from evaluate import load
-import torch
-import transformers
 
 import csv
 
@@ -25,12 +28,31 @@ with open("generifs1000.tsv") as file:
 print("Corpus size: " + str(len(corpus)))
 
 
-def tokenize():
+def tokenize(input_str):
+    result = []
     bpt = pt.BertPreTokenizer()
-    print(bpt.pre_tokenize_str("this is a test, and hello world."))
+    for ele in bpt.pre_tokenize_str(input_str):
+        if ele[0] not in string.punctuation:
+            result.append(ele[0])
+    return result
 
 
-tokenize()
+max1_len = 0
+max2_len = 0
+
+for gene_rif in corpus:
+    tokens = tokenize(input_str=gene_rif)
+    current_len = len(tokens)
+    if max1_len < current_len:
+        max2 = max1
+        max1 = tokens
+        max2_len = max1_len
+        max1_len = current_len
+    elif max2_len < current_len:
+        max2 = tokens
+        max2_len = current_len
+
+print("Highest number of token in geneRIF: " + str(max1_len))
 
 perplexity = load("perplexity", module_type="measurement")
 bbu_results = perplexity.compute(add_start_token=False, data=corpus, model_id='bert-base-uncased')
--- step1/NLP_Assignment_1_4.py
+++ step1/NLP_Assignment_1_4.py
@@ -6,6 +6,20 @@ from nltk.tokenize import word_tokenize
 nltk.download('stopwords')
 nltk.download('punkt')
 
+import csv
+
+corpus = []
+
+is_FirstLine = False
+
+with open("generifs1000.tsv") as file:
+    tsv_file = csv.reader(file, delimiter="\t")
+
+    for line in tsv_file:
+        if is_FirstLine:
+            corpus.append(line[4])
+        is_FirstLine = True
+
 
 # Finish me
 def getJaccard(tokenset1, tokenset2):
@@ -24,16 +38,31 @@ def getJaccard(tokenset1, tokenset2):
 
 stop_words = set(stopwords.words('english'))
 
-tk = TweetTokenizer()
-tk_tokens = tk.tokenize("This is a test")
-filtered_tk_tokens = [w for w in tk_tokens if not w in stop_words]
-print(tk_tokens)
-print(filtered_tk_tokens)
 
-wt_tokens = word_tokenize("This is a test sample")
-filtered_wt_tokens = [w for w in wt_tokens if not w in stop_words]
-print(word_tokenize("This is a test"))
-print(filtered_wt_tokens)
+def tokenizeByTweetTokenizer(input_str):
+    tk = TweetTokenizer()
+    tk_tokens = tk.tokenize(input_str)
+    filtered_tk_tokens = [w for w in tk_tokens if not w in stop_words]
+    return filtered_tk_tokens
 
-distance = getJaccard(filtered_wt_tokens, filtered_tk_tokens)
-print(distance)
+
+def tokenizeByWord_tokenize(input_str):
+    wt_tokens = word_tokenize(input_str)
+    filtered_wt_tokens = [w for w in wt_tokens if not w in stop_words]
+    return filtered_wt_tokens
+
+
+max_distance = 0
+max_distance_str = ""
+
+for geneRIF in corpus:
+    tk_output = tokenizeByTweetTokenizer(geneRIF)
+    wt_output = tokenizeByWord_tokenize(geneRIF)
+    distance = getJaccard(tk_output, wt_output)
+    if distance > max_distance:
+        max_distance = distance
+        max_distance_str = geneRIF
+
+print(max_distance_str)
+print(tokenizeByWord_tokenize(max_distance_str))
+print(tokenizeByTweetTokenizer(max_distance_str))
